2dc7f1ab9a8ca038850e363c49fa53c7bcf570d6
nanna <dannasandez0@gmail.com>
2024-12-09 15:06:14 -0800

    Un commit pequeño

--- Presentacion/PaginasMaestras/mpConLogueo.master.designer.cs
+++ Presentacion/PaginasMaestras/mpConLogueo.master.designer.cs
@@ -42,6 +42,24 @@ namespace Presentacion.PaginasMaestras
         protected global::System.Web.UI.WebControls.LinkButton lbHistorialVentas;
 
         /// <summary>
+        /// Control LinkButton1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
+
+        /// <summary>
+        /// Control LinkButton2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton2;
+
+        /// <summary>
         /// Control lbLogOut.
         /// </summary>
         /// <remarks>
