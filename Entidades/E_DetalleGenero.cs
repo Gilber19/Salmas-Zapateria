53947f80bf0e0f2ce33a2aae1eaa609cf8c0d406
Daniel Solano <solanomezaangel@gmail.com>
2024-12-08 22:18:27 -0800

    Entidades Genero y DetalleGenero

---
+++ Entidades/E_DetalleGenero.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    internal class E_DetalleGenero
+    {
+        #region Atributos
+        private int idDetalleGenero;
+        private int idGenero;
+        private int idArticulo;
+        #endregion
+
+        #region Constructores
+        public E_DetalleGenero(int idDetalleGenero, int idGenero, int idArticulo)
+        {
+            IdDetalleGenero = idDetalleGenero;
+            IdGenero = idGenero;
+            IdArticulo = idArticulo;
+        }
+
+        public E_DetalleGenero()
+        {
+            IdDetalleGenero = idDetalleGenero;
+            IdGenero = idGenero;
+            IdArticulo = idArticulo;
+        }
+        #endregion
+
+        #region Encapsulamientos
+        public int IdDetalleGenero { get => idDetalleGenero; set => idDetalleGenero = value; }
+        public int IdGenero { get => idGenero; set => idGenero = value; }
+        public int IdArticulo { get => idArticulo; set => idArticulo = value; }
+
+        #endregion
+    }
+}
---
+++ Entidades/E_Genero.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    internal class E_Genero
+    {
+        #region Atributos
+        private int idGenero;
+        private string genero;
+        #endregion
+
+        #region Constructores
+        public E_Genero(int idGenero, string genero)
+        {
+            IdGenero = idGenero;
+            Genero = genero;
+        }
+
+        public E_Genero()
+        {
+           IdGenero = idGenero;
+           Genero = genero;
+        }
+        #endregion
+
+        #region Encapsulamientos
+        public int IdGenero { get => idGenero; set => idGenero = value; }
+        public string Genero { get => genero; set => genero = value; }
+        #endregion
+
+
+
+
+
+
+    }
+}
